1bf0f760d1df79240006287c066214a49a63dcfd
KingChlorine <kingchloe281@gmail.com>
2025-11-14 13:03:13 +0000

    teps to making gallery with nav buttons

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ document.getElementById("sign").addEventListener("click", function() {
       alert("Button clicked!");
     });
 
+  var myImage=document.querySelector("#imageid");
 
+  function FirstImage()
+{
+  myImage.src="images/shop.png";
+}
+
+function SecondImage()
+{
+  myImage.src="images/building pc.png";
+}
 
